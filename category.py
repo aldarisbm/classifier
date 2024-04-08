6741a7d4c7badbb6891ee56cb92f9b61d1efa1cf
Jose Berrio <aldarisbm@gmail.com>
2024-04-07 21:39:39 -0400

    pushing

--- category.py
+++ category.py
@@ -1,14 +1,19 @@
 from dataclasses import dataclass
 from typing import List
 
-from jinja2 import Template
+from jinja2 import Template, Environment, FileSystemLoader
 from pydantic import BaseModel
 
 
 @dataclass
 class Prompt:
-    template: Template
-    kwargs: dict
+    template_path: str
+    template_args: dict
+
+    def load_template(template: str) -> Template:
+        environment: Environment = Environment(loader=FileSystemLoader("prompt_templates/"))
+        ptpl: Template = environment.get_template(f"{template}.j2")
+        return ptpl
 
 
 @dataclass
