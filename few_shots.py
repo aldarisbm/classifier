df01d3bb54c74bf78c3c4d087695cb2b0cf8ea24
Jose Berrio <aldarisbm@gmail.com>
2024-04-28 10:32:37 -0400

    updated prompt

--- few_shots.py
+++ few_shots.py
@@ -1,46 +1,46 @@
 industry = [
     dict(
         prompt="What are the latest trends on silicone chip technology?",
-        response="{\"label\": \"Technology\"}"
+        response="Technology"
     ),
     dict(
         prompt="What kind of new harvesting technology is available? Has there been any new breakthroughs?",
-        response="{\"label\": \"Agriculture\"}"
+        response="Agriculture"
     ),
     dict(
         prompt="What kind of new software development tools are there? Are there any interesting trends in AI?",
-        response="{\"label\": \"Technology\"}"
+        response="Technology"
     ),
     dict(
         prompt="What are some recent advancements in gene editing? Are there any new drugs in development for cancer treatment?",
-        response="{\"label\": \"Healthcare\"}"
+        response="Healthcare"
     ),
     dict(
         prompt="What are the new regulations for cryptocurrency? How is the stock market performing?",
-        response="{\"label\": \"Finance\"}"
+        response="Finance"
     ),
     dict(
         prompt="What are the challenges of automating car assembly lines? How can manufacturers improve supply chain efficiency?",
-        response="{\"label\": \"Manufacturing\"}"
+        response="Manufacturing"
     ),
     dict(
         prompt="What are some effective omnichannel marketing strategies? How can e-commerce businesses improve customer experience?",
-        response="{\"label\": \"Retail\"}"
+        response="Retail"
     ),
     dict(
         prompt="What are some new developments in educational technology? How can we improve online learning platforms?",
-        response="{\"label\": \"Education\"}"
+        response="Education"
     ),
     dict(
         prompt="What are the benefits of solar energy? How can we achieve energy independence?",
-        response="{\"label\": \"Energy\"}"
+        response="Energy"
     ),
     dict(
         prompt="How can we improve public transportation systems? What are some new initiatives in urban planning?",
-        response="{\"label\": \"Government & Public Administration\"}"
+        response="Government & Public Administration"
     ),
     dict(
         prompt="What are some new technologies used in precision agriculture? How can we improve farm efficiency?",
-        response="{\"label\": \"Agriculture\"}"
+        response="Agriculture"
     ),
 ]
